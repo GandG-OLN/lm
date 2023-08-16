74f0991669b94ccf716bafb9d44d784a8154afd2
GandG-OLN <olivier.nadieline@gandgcorp.com>
2023-08-16 10:00:51 +0000

    [INIT]

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Lettre de mission',
+    'summary': """Module pour automatiser les LM""",
+    'version': '14.0.0',
+    'description': """Lettre de Mission est un module pour faciliter à la team admin la rédaction des lettres de mission""",
+    'author': 'GandG-OLN',
+    'company': 'G&G Professional Services',
+    'website': 'https://www.gandgcorp.com',
+    'category': 'Tools',
+    'depends': ['base'],
+    'license': 'AGPL-3',
+    'data': [
+ #       'views/etudiant_info.xml',
+ #       'views/classe_info.xml',
+ #       'views/matiere_info.xml',
+ #       'security/ir.model.access.csv',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
